2b49f4645108155786e6eaf0c443f1b602b73e1a
Mingche <132795672+MingcheL1@users.noreply.github.com>
2024-06-07 20:22:48 -0500

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,7 @@
+oh-my-posh init pwsh --config 'C:\Users\Path\To\Your\Posh\Theme\night-owl.omp.json' | Invoke-Expression
+Write-Host 'Powershell' $PsVersionTable.PSVersion '-' (Get-date)
+Write-Host "++++++++++++++++++++++++++"
+Write-Host "    Username's Terminal"
+Write-Host "++++++++++++++++++++++++++"
+Write-Host " "
+
