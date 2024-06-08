6952560c5a68607b0253a4bb8e12c955feebc7fc
Mingche <132795672+MingcheL1@users.noreply.github.com>
2024-06-07 22:54:55 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,7 +1,40 @@
-oh-my-posh init pwsh --config 'C:\Users\Path\To\Your\Posh\Theme\night-owl.omp.json' | Invoke-Expression
+
+oh-my-posh init pwsh --config 'C:\Users\Your\Path\To\Posh\Theme\night-owl.omp.json' | Invoke-Expression
 Write-Host 'Powershell' $PsVersionTable.PSVersion '-' (Get-date)
 Write-Host "++++++++++++++++++++++++++"
-Write-Host "    Username's Terminal"
+Write-Host "    User's Terminal"
 Write-Host "++++++++++++++++++++++++++"
 Write-Host " "
+
+function update{
+	.$profile
+	clr
+}
+	
+function clr{
+	clear
+	Write-Host 'Powershell' $PsVersionTable.PSVersion '-' (Get-date)
+	Write-Host "++++++++++++++++++++++++++"
+	Write-Host "    User's Terminal"
+	Write-Host "++++++++++++++++++++++++++"
+	Write-Host " "
+	
+}
+function project{
+	code \Your\Path\To\Your\Project
+
+}
+function bg-custom {
+    Set-WindowsTerminalBackgroundImage -BackgroundPath PATHTOBGS -All
+}
+
+
+function bg-random{
+    Set-WindowsTerminalBackgroundImage -RandomBackgroundFolder PATHTOBGS -All
+}
+
+
+
+
+
 
